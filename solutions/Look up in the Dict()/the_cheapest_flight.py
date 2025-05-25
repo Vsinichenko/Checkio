27c7e994185ccbd297b040fc0d4ae0cd807497b3
Valentina Sinichenko <valentyna.sinichenko@mailbox.tu-dresden.de>
2025-05-25 17:08:28 +0200

    the cheapest flight: partial solution

--- solutions/Look up in the Dict()/the_cheapest_flight.py
+++ solutions/Look up in the Dict()/the_cheapest_flight.py
@@ -19,33 +19,87 @@
 # 
 # END_DESC
 
+import math
+
+def get_max_trip_length(num_cities:int)-> int:
+    max_lenth=0
+    for r in range(2, num_cities+1):
+        max_lenth+=math.perm(num_cities, r)
+    return max_lenth
+
+
+assert get_max_trip_length(4)==12+24+24
+assert get_max_trip_length(3)==12
+
+
+def is_meaningful_route(trip_1: str, trip_2: str) -> bool:
+    for city in trip_1[:-1]:
+        if city in trip_2:
+            return False
+    return True
+
+
 def cheapest_flight(costs: list, a: str, b: str) -> int:
     cities = set()
     routes = set()
+    simple_routes_dict = {}
+
     for cost in costs:
-        direct_route = cost[0]+cost[1]
-        return_route = cost[1]+cost[0]
+        start_city = cost[0]
+        end_city = cost[1]
+        price=cost[-1]
+        direct_route =  start_city+end_city
+        return_route = end_city+start_city
         routes.add(direct_route)
         routes.add(return_route)
-        cities.add(cost[0])
-        cities.add(cost[1])
+        cities.add(start_city)
+        cities.add(end_city)
+
+        if start_city in simple_routes_dict:
+            simple_routes_dict[start_city][end_city]=price
+        else:
+            simple_routes_dict[start_city]={}
+            simple_routes_dict[start_city][end_city]=price
+
+        if end_city in simple_routes_dict:
+            simple_routes_dict[end_city][start_city]=price
+        else:
+            simple_routes_dict[end_city]={}
+            simple_routes_dict[end_city][start_city]=price
+
+    simple_routes = routes.copy()
         
-    for i in rane(len(costs)*2): # TODO how long can it be
-        for begin_route in routes:
-            for end_route in routes:
+    for i in range(get_max_trip_length(len(cities))+1):
+        for begin_route in routes.copy():
+            for end_route in simple_routes:
                 # AFHB and BD combine into AFHBD
-                if begin_route[-1]==end_route[0]:
+                if begin_route[-1]==end_route[0] and is_meaningful_route(begin_route, end_route):
                     routes.add(begin_route[:1]+end_route)
        
-     costs_dict = dict.fromkeys(routes, None) 
-                           
-                           
-        
-        
-        
-        
-        
+    costs_dict = dict.fromkeys(routes, None) 
+    # calculate the costt of each trip 
+    for route in costs_dict.keys(): # TODO fix price calculation here
+        cost=0
+        # key example ABC
+        for i, _ in enumerate(route[1:]): # start from second city D
+            price = simple_routes_dict[route[i]][route[i+1]]
+            cost+=price
+        costs_dict[route]=cost
 
+    # now, get the cheapest trip
+    possible_trips = []
+    for route in costs_dict.keys():
+        if route.startswith(a) and route.endswith(b):
+            possible_trips.append(route)
+
+    if not possible_trips:
+        return 0
+
+    min_cost = costs_dict[possible_trips[0]]
+    for trip in possible_trips:
+        if costs_dict[trip]<min_cost:
+            min_cost=costs_dict[trip]
+    return min_cost   
 
 print("Example:")
 print(cheapest_flight([["A", "C", 100], ["A", "B", 20], ["B", "C", 50]], "A", "C"))
